0a716ea532d0eddbe5c4c20e6e44b46ca014db2c
Ayush <ayushgadag09@gmail.com>
2026-02-10 20:56:43 +0530

    logs

---
+++ log.py
@@ -0,0 +1,23 @@
+logs = [
+    "INFO User login",
+"ERROR Database failed",
+"WARNING Disk full",
+"ERROR Timeout"
+]
+
+count =  0
+
+for i in logs:
+    if "ERROR" in i:
+        print(i)
+        count+=1
+        print(f"the total number of count is :-{count}")
+        
+
+
+
+        
+
+
+
+
